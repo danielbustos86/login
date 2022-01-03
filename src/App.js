d99aa20c28e98fa31874f28bcb2f920661d0c287
daniel.bustos <daniel.bustos@mundopacifico.cl>
2022-01-03 18:27:56 -0300

    pasos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn';
+import Menu from './Menu';
+import { useState, useEffect } from 'react';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Switch, Route, Link, Navigate, Redirect } from "react-router-dom";
+
+const browserHistory = createBrowserHistory();
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={browserHistory}>
+      <Switch>
+        <Redirect from="/" exact={true} to="/login" />
+        <Route path="/login">
+          <SignIn />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+
+
+
+        {/* <Route path="/menu" component={<Menu/>} /> */}
+      </Switch>
+
+
+    </Router>
+
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+import Checkout from './Pasos/Checkout';
 
+
+
+
+axios.interceptors.request.use(config=>{
+  
+  config.baseURL = "http://localhost:5000"
+  
+  const token = localStorage.getItem('TOKEN_APP_TALLER');
+
+  if(token!=null)
+  {
+    config.headers.Authorization =  `Bearer ${token}`;
+  }else{
+    config.headers.Authorization = null;
+  }
+
+ 
+  return config
+})
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Checkout />
   </React.StrictMode>,
   document.getElementById('root')
 );
